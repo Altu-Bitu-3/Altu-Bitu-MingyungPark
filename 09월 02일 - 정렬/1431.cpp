85dcc7e07a24b1c25100c7cfd446ee8e5d1d92f8
Kate Min Gyung Park <kateking001130@ewhain.net>
2022-09-10 23:42:49 +0900

    [정렬]09월 02일 - Update
    1431번 업데이트 했습니다.

--- 09월 02일 - 정렬/1431.cpp
+++ 09월 02일 - 정렬/1431.cpp
@@ -9,7 +9,7 @@ int sumNumber(const string& a) {
 	int sum = 0;
 	for (int i = 0; i < a.length(); i++)
 	{
-		if (a[i] <= '9')
+		if (a[i]>='0' && a[i] <= '9')
 			sum += a[i] - '0';
 	}
 	return sum;
