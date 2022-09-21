1c6f96085794d1ccd5b7b44317d9b7a63b1fb75b
Kate Min Gyung Park <kateking001130@ewhain.net>
2022-09-22 00:03:01 +0900

    [스텍,큐,덱] 9월 9일 - Update

--- 09월 09일 - 스텍,큐,덱/18115.cpp
+++ 09월 09일 - 스텍,큐,덱/18115.cpp
@@ -1,38 +1,39 @@
 #include <iostream>
 #include <stack>
 #include <deque>
+
 using namespace std;
 
 // dq덱에 초기카드상태 저장하는 함수
-void card(deque<int>& dq, stack<int>& s, int n) {
+deque<int> card(stack<int>& s, int n) {
 	int tmp;
-	
+	deque<int>ans;
 	//1번카드부터 n번 카드까지 순서대로 dq에 삽입
 	for (int i = 1; i <= n; i++) {
 		//i번 카드를 놓을 때 쓴 기술이 s.top()
 		switch (s.top()) {
 			//1번기술 : 제일 위의 카드를 내려놓음 >> 제일위로 카드 이동(push_front)
 		case 1:
-			dq.push_front(i);
+			ans.push_front(i);
 			break;
 			
 			//2번 기술 :위에서 두번째 자리로 카드 이동
 		case 2:
-			tmp = dq.front();
-			dq.pop_front();
-			dq.push_front(i);
-			dq.push_front(tmp);
+			tmp = ans.front();
+			ans.pop_front();
+			ans.push_front(i);
+			ans.push_front(tmp);
 			break;
 
 			//3번 기술 : 맨 아래로 카드 이동
 		case 3:
-			dq.push_back(i);
+			ans.push_back(i);
 			break;
 		}
 		//사용한 기술 pop
 		s.pop();
 	}
-	return;
+	return ans;
 }
 
 int main() {
@@ -50,7 +51,7 @@ int main() {
 	}
 
 	//초기상태 dq에 저장
-	card(dq, s, n);
+	dq=card( s, n);
 
 	//앞에서부터dq cout
 	while (n--) {
